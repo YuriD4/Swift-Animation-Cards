e38afb456bc8738acdfa6badf8ce4675e1ae1954
Yuri Chukhlib <yuradeveloper4@gmail.com>
2016-06-16 01:10:29 +0300

    Added animation during the segue between controllers

---
+++ Swift-Animation-Cards/Swift-Animation-Cards/DetailVC.swift
@@ -0,0 +1,24 @@
+//
+//  DetailVC.swift
+//  Swift-Animation-Cards
+//
+//  Created by Yura Chukhlib on 16.06.16.
+//  Copyright © 2016 Yuri Chukhlib. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+}
--- Swift-Animation-Cards/Swift-Animation-Cards/HomeVC.swift
+++ Swift-Animation-Cards/Swift-Animation-Cards/HomeVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Spring
 
 class HomeVC: UIViewController {
 
@@ -25,12 +26,21 @@ class HomeVC: UIViewController {
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var favouritesLabel: UIImageView!
 
+    struct Constants {
+        static let screenWidth = UIScreen.mainScreen().bounds.width
+        static let screenHeight = UIScreen.mainScreen().bounds.height
+    }
 
     @IBAction func likeButtonDidPress(sender: UIButton) {
     }
 
     @IBAction func shareButtonDidPress(sender: UIButton) {
         shareView.hidden = false
+        shareView.alpha = 0
+        
+        UIView.animateWithDuration(0.5, animations: {
+            self.shareView.alpha = 1
+        })
     }
 
     @IBAction func userButtonDidPress(sender: UIButton) {
@@ -38,12 +48,30 @@ class HomeVC: UIViewController {
     }
     
     @IBAction func imageButtonDidPress(sender: UIButton) {
+        
+        UIView.animateWithDuration(0.7, animations: {
+            self.dialogView.layer.cornerRadius = 0
+            self.dialogView.frame = CGRectMake(0, 0, Constants.screenWidth, Constants.screenHeight)
+            let scale = self.imageButton.frame.height / self.imageButton.frame.width
+            self.imageButton.frame = CGRectMake(0, 0, Constants.screenWidth, Constants.screenWidth * scale)
+            self.likeButton.alpha = 0
+            self.shareButton.alpha = 0
+            self.userButton.alpha = 0
+            self.headerView.alpha = 0
+            }, completion: { finished in
+                self.performSegueWithIdentifier("homeToDetail", sender: self)
+        })
     }
     
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        insertBlurView(backgroundMaskView, style: UIBlurEffectStyle.Dark)
+        insertBlurView(headerView, style: UIBlurEffectStyle.Dark)
         
     }
 }
